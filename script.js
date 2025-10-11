8b540fae8ab28a6f8b4ddd123fb1d716305cc147
AshishV1572 <ashishvaria231083@gmail.com>
2025-10-11 17:40:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,14 +17,79 @@ function updateDateTime() {
 // ---------- Dropdown Data ----------
 const data = {
   India: {
-    Gujarat: {
-      PanchMahals: {
-        Kalol: {
-          Bhavanadasi: ["Kamaladasi", "Kailashdasi", "Kishnadasi", "Payaldasi"],
+        Gujarat: {
+          PanchMahals: {
+            Kalol: {
+              Bhavanadasi: [
+                "Kamaladasi", "Kailashdasi", "Kishnadasi", "Payaldasi", "Tanvidas",
+                "Savitadasi(Kalol)", "Sushiladasi(Kalol)", "Jinaldaasi(Kalol)",
+                "Pujadasi", "Pinnacle Dasi Halol", "Sheetaldasi Ghoghamba",
+                "Sudhadasi Kalol", "Varshadasi Halol"
+              ],
+              Krishnadasi: [
+                "Jyotidasi(Ghoghamba)", "Rajnidasi(Halol)", "Kasidasi",
+                "Bhavanadasi(Kalol)", "Gitadasi", "Jinaldasi(Halol)",
+                "Minadasi(Kalyana)", "Minadasi(Godhra)", "Urmiladasi",
+                "Aashadasi", "Kailashdasi Godhra", "Sadhanadasi Halol",
+                "Mayadasi Kalol", "Kishanadasi Godhra"
+               ],
+               Nayandas: [
+                "Shantilaldas", "MahipalDas", "DharmeshDas", "Jhondas"
+               ],
+               Dharmeshdas: [
+                "Ashishdas", "ManojDas", "NayanDas", "Arvind Das"
+              ]
+            },
+            Godhra: {
+              Babudas: [
+                "Jitendra Das", "Ashok Das", "Janadas", "Manoj Das", "Mukeshdas",
+                "Mukundandas", "Doltadas", "Tejasdas", "Narendradas", "Babudas",
+                "Bharatdas", "Jagadishdas", "Prakashdas", "Shankardas",
+                "Satishdas", "Rakeshdas"
+              ]
+            },    
+             Halol: {
+               Deepakdas: [
+                "Kanudas", "Bhagwandas", "Arjundas", "Jaydas", "Rajudas"
+               ],
+               Jaydas: [
+                "Maheshdas", "Sahdevdas", "Jagadishdas", "Deepak Das", 
+               ]
+            },    
+             MorvaHadaf: {
+               Nileshdas: [
+                "Balwantdas", "Chandudas", "fatehsinghdas", "Sahdevdas", "Dharmeshdas", "Rameshdas"
+                ],
+             Balvantdas: [
+                "Nileshdas", "Kanudas", "Vinudas", "Babudas", "Kadudas", "Purushottamdas"
+                ]
+             }, 
+              Ghoghamba: {
+                Bharatdas: [
+                "Ishwardas", "AshokDas", "Arvinddas", "Girishdas", "Sanjaydas", "Bharatdas"
+              ]
+            },  
+              Shahera: {
+               Nileshdas: [
+              "Hasmukhdas", "Pruthvidas", "BhalasihDas", "RameshDas", "ShankarDas", "Natudas", "Mithadas", "MangaDas",
+              "Rajesh Das", "Pankajdas", "Nileshdas", "Laldas"
+              ],
+               Hsamukhdas : [
+              "chandudas", "AlpeshDas", "MotiDas", "Rajudas", "Artadas", "Arvatadas", "Arvind Das", "Kirandas",
+              "Tinadas", "Bhalladas", "Fuladas"
+               ]
+             }, 
+          Jambughoda: {},
+          },
+          Ahmedabad: {}, Amreli: {}, Anand: {}, Aravalli: {}, Banaskantha: {}, Bharuch: {},
+          Bhavnagar: {}, Botad: {}, Chhotaudepur: {}, Dahod: {}, Dangs: {}, Devbhumi_Dwarka: {},
+          Gandhinagar: {}, Gir_Somnath: {}, Jamnagar: {}, Junagadh: {}, Kachchh: {}, Kheda: {},
+          Mahesana: {}, Mahisagar: {}, Morbi: {}, Narmada: {}, Navsari: {}, Patan: {},
+          Porbandar: {}, Rajkot: {}, Sabarkantha: {}, Surat: {}, Surendranagar: {},
+          Tapi: {}, Vadodara: {}, Valsad: {}
         },
-      },
-    },
-  },
+        Maharashtra: {}
+    
 };
 
 // ---------- Helper Functions ----------
@@ -150,6 +215,7 @@ function saveTableToSheet() {
   });
   alert("✅ Table data sent to Google Sheet!");
 }
+
 
 
 
